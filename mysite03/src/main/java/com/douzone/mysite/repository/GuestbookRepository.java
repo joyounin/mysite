597f9858e494986228db78c36139f8be780c906b
joyounin <3723519@naver.com>
2023-02-02 14:55:48 +0900

    aop: measure execution time

--- mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
@@ -3,10 +3,10 @@ package com.douzone.mysite.repository;
 import java.util.List;
 import java.util.Map;
 
-
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
 
 import com.douzone.mysite.vo.GuestbookVo;
 
@@ -18,6 +18,7 @@ public class GuestbookRepository {
 	
 	public List<GuestbookVo> findAll() {
 		return sqlSession.selectList("guestbook.findAll");
+	 
 	}
 
 	public void insert(GuestbookVo vo) {
