da87a9c12a2cbd6384ba2715d823aa078cf8c799
joyounin <3723519@naver.com>
2023-02-03 11:28:15 +0900

    Bean Validation

--- mysite03/src/main/java/com/douzone/mysite/vo/UserVo.java
+++ mysite03/src/main/java/com/douzone/mysite/vo/UserVo.java
@@ -1,12 +1,25 @@
 package com.douzone.mysite.vo;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class UserVo {
+	
+	
 	private Long no;
+	@NotEmpty
+	@Length(min=2,max=8)
 	private String name;
+	
+	@NotEmpty
+	@Email
 	private String email;
+	@NotEmpty
+	@Length(min=2, max=8)
 	private String password;
 	private String gender;
-
+	
 	public Long getNo() {
 		return no;
 	}
