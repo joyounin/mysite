0f5db093569d63986aae0544b93a30a4e4a70732
joyounin <3723519@naver.com>
2023-02-14 11:17:20 +0900

    mysite05: ServletContainerInitializer #1

---
+++ mysite05/src/main/java/com/douzone/mysite/interceptor/SiteInterceptor.java
@@ -0,0 +1,35 @@
+package com.douzone.mysite.interceptor;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.douzone.mysite.service.SiteService;
+import com.douzone.mysite.vo.SiteVo;
+
+public class SiteInterceptor implements HandlerInterceptor {
+	
+	@Autowired
+	private SiteService siteService;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		//2023.02.08 강사님 코드
+		SiteVo sitevo = (SiteVo)request.getServletContext().getAttribute("siteVo");
+		if(sitevo == null) {
+			siteService.getSite();
+			request.getServletContext().setAttribute("sitevo", sitevo);
+		}
+//		2023.02.07 내가 짠 코드
+//		SiteVo site = siteService.getSite();
+//		servletContext.setAttribute("site", site);
+//		
+		return true;
+	}
+	
+}
